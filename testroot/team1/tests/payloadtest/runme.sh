b468f2bcdc37340932e9b3dec1e37b433f262458
R <rod@nayfield.com>
2019-01-30 17:41:32 -0500

    test includes subdir

--- testroot/team1/tests/payloadtest/runme.sh
+++ testroot/team1/tests/payloadtest/runme.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # a test that shows how payloads come along
 
-grep -q 'themagicstring' ./payload && exit 0 || exit 1
+grep -q 'themagicstring' ./payload  || exit 1
+grep -q 'yetanotherstring' ./somedir/another_payload || exit 1
+exit 0
